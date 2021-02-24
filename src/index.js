ff919d9265c9210e1a3ea533652d7a834dd3f509
Alexander Leonov <spawnleon@gmail.com>
2021-02-24 21:44:03 +0300

    feat: tasks have been finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+    const stack = [];
+
+    let newStr = str.split('');
+    newStr.forEach((bracket, index) => {
+        if (index === 0) {
+            stack.push(bracket);
+            return;
+        }
+
+        let lastBracket = bracketsConfig
+            .find(typeBracket => stack[stack.length - 1] === typeBracket[0]);
+        lastBracket = lastBracket && lastBracket[1];
+
+        if (bracket !== lastBracket) {
+            stack.push(bracket);
+        } else {
+            stack.pop();
+        }
+    });
+
+    return stack.length === 0;
+};
